50ac51d30310c42f2f2843dc5259cdc8fc9d83c7
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2020-10-12 18:41:21 +0700

    add model genres

---
+++ lib/models/Genre.dart
@@ -0,0 +1,27 @@
+class Genre {
+  final int id;
+  final String name;
+
+  Genre({ this.id, this.name });
+}
+
+List<Genre> genres = [
+  Genre( id: 28, name: 'Action' ),
+  Genre( id: 12, name: 'Adventure' ),
+  Genre( id: 16, name: 'Animation' ),
+  Genre( id: 35, name: 'Comedy' ),
+  Genre( id: 80, name: 'Crime' ),
+  Genre( id: 99, name: 'Documentary' ),
+  Genre( id: 18, name: 'Drama' ),
+  Genre( id: 10751, name: 'Family' ),
+  Genre( id: 14, name: 'Fantasy' ),
+  Genre( id: 27, name: 'Horror' ),
+  Genre( id: 10402, name: 'Music' ),
+  Genre( id: 9648, name: 'Mistery' ),
+  Genre( id: 10749, name: 'Romance' ),
+  Genre( id: 878, name: 'Science Fiction' ),
+  Genre( id: 10770, name: 'TV Movie' ),
+  Genre( id: 53, name: 'Thriller' ),
+  Genre( id: 10752, name: 'War' ),
+  Genre( id: 37, name: 'Western' ),
+];
